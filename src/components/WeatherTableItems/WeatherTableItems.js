eaf2e953980c6ea3f2dab97b65db43f0903278b3
metinars <arsmetin90@gmail.com>
2023-09-05 17:01:59 +0300

    weatherTableItems module css added

--- src/components/WeatherTableItems/WeatherTableItems.js
+++ src/components/WeatherTableItems/WeatherTableItems.js
@@ -1,35 +1,39 @@
 import React from 'react';
 
-import './WeatherTableItems.css';
+// import './WeatherTableItems.css';
+import styles from './WeatherTableItems.module.css'
+import Card from '../../Ui/Card/Card';
 
 const WeatherTableItems = (props) => {
   // console.log(props);
   return (
-    <table className='table'>
-      <thead id='2' className='table-dark'>
-        <tr>
-          <th scope='col'>Tarih</th>
-          <th scope='col'>Hava Durumu</th>
-          <th scope='col'>Durum</th>
-          <th scope='col'>Sıcaklık</th>
-        </tr>
-      </thead>
-      {props.weatherItemData.map((data) => (
-        <tbody key={Math.random().toString()}>
+    <Card className={styles.table}>
+      <table className='table'>
+        <thead id='2' className='table-dark'>
           <tr>
-            <td className='noBorder'>{data.date}</td>
-            <td className='noBorder'>{data.day.condition.text}</td>
-            <td className='noBorder'>
-              <img
-                src={data.day.condition.icon}
-                alt='Weather data by WeatherAPI.com'
-              ></img>
-            </td>
-            <td className='noBorder'>{data.day.avgtemp_c}</td>
+            <th scope='col'>Tarih</th>
+            <th scope='col'>Hava Durumu</th>
+            <th scope='col'>Durum</th>
+            <th scope='col'>Sıcaklık</th>
           </tr>
-        </tbody>
-      ))}
-    </table>
+        </thead>
+        {props.weatherItemData.map((data) => (
+          <tbody key={Math.random().toString()}>
+            <tr>
+              <td className='noBorder'>{data.date}</td>
+              <td className='noBorder'>{data.day.condition.text}</td>
+              <td className='noBorder'>
+                <img
+                  src={data.day.condition.icon}
+                  alt='Weather data by WeatherAPI.com'
+                ></img>
+              </td>
+              <td className='noBorder'>{data.day.avgtemp_c}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </Card>
   );
 };
 
